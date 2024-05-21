5f00781d196958ea8e6e1e196289cc1daee1e399
matteo serva <matteo.serva@gmail.com>
2024-05-21 22:19:41 +0200

    sequence executor

--- exec.py
+++ exec.py
@@ -3,8 +3,8 @@ import sys
 from utils.common import get_input,get_formatter,readfile,build_prompt
 from utils.client import Client
 from utils.formatter import Formatter,PromptBuilder
-from utils.parser import solve_templates
-from utils.executor import StatelessExecutor
+
+from utils.executor import SequenceExecutor
 
 
 
@@ -21,61 +21,13 @@ parameters["penalize_nl"] = False
 parameters["seed"] = -1
 client.set_parameters(parameters)
 
-class SequenceExecutor:
-    def __init__(self,client):
-        #builder = PromptBuilder()
-        #builder.load_model(client.get_model_name())
-        #self.builder=builder
-        self.client = client
-        self.execRow = StatelessExecutor(client)
 
-    def load_config(self,cl_args=None):
-        for i,el in enumerate(cl_args):
-            try:
-                cl_args[i] = readfile(el)
-            except:
-                pass
-        for el in cl_args[1:]:
-            cl_args[0].replace("{}",el,1)
-        return cl_args
-
+        
 seqExec = SequenceExecutor(client)
 execRow = seqExec.execRow
-
-def execute_row(inputs,variables={}):
-    for i in range(len(inputs)):
-        try:
-            p=readfile(inputs[i])
-            inputs[i] = p
-        except:
-            pass
-    m,_ = solve_templates(inputs[0],inputs[1:],variables)
-    res = execRow(m)
-
-    return res
-
-def execute_list(instructions, cl_args):
-    pos = 1
-    variables={}
-    for i,v in enumerate(cl_args):
-        idx = i+1
-        stri = "c" + str(idx)
-        variables[stri] = v
-    for instr in instructions:
-        res = execute_row(instr,variables)
-        fn = "/tmp/llm_exec_" + str(pos) + ".txt"
-        f = open(fn,"w")
-        f.write(res)
-        f.close()
-        variables[str(pos)] = res
-        pos = pos + 1
-
-
 cl_args = seqExec.load_config(sys.argv[1:])
-lista_istruzioni = cl_args[0]
 
-instructions = [ [ es  for es in el.strip().split(" ") if len(es) > 0] for el in lista_istruzioni.strip().split("\n")]
+seqExec(cl_args[1:])
 
-execute_list(instructions, cl_args[1:])
 
 
--- utils/executor.py
+++ utils/executor.py
@@ -1,4 +1,6 @@
 from .formatter import PromptBuilder
+from .parser import solve_templates
+from .common import readfile
 
 def send_chat(builder,client):
     r = client.send_prompt(builder)
@@ -35,7 +37,6 @@ class StatefulExecutor:
         builder.load_model(client.get_model_name())
         self.builder=builder
         self.client = client
-
         self.print_prompt = True
 
     def __call__(self,m):
@@ -51,5 +52,58 @@ class StatefulExecutor:
         return res
     
 class SequenceExecutor:
-    pass
-    #TODO
+    def __init__(self,client):
+
+        self.client = client
+        self.execRow = StatelessExecutor(client)
+
+    def load_config(self,cl_args=None):
+        for i,el in enumerate(cl_args):
+            try:
+                cl_args[i] = readfile(el)
+            except:
+                pass
+        for el in cl_args[1:]:
+            cl_args[0].replace("{}",el,1)
+        self.instructions_raw = cl_args[0]
+        return cl_args
+
+    def _execute_row(self,inputs,variables={}):
+        for i in range(len(inputs)):
+            try:
+                p=readfile(inputs[i])
+                inputs[i] = p
+            except:
+                pass
+        m,_ = solve_templates(inputs[0],inputs[1:],variables)
+        res = self.execRow(m)
+        return res
+    
+    def _execute_list(self,instructions, cl_args):
+        pos = 1
+        variables={}
+        res = None
+        for i,v in enumerate(cl_args):
+            idx = i+1
+            stri = "c" + str(idx)
+            variables[stri] = v
+        for instr in instructions:
+            res = self._execute_row(instr,variables)
+            fn = "/tmp/llm_exec_" + str(pos) + ".txt"
+            f = open(fn,"w")
+            f.write(res)
+            f.close()
+            variables[str(pos)] = res
+            pos = pos + 1
+        return res
+
+    def __call__(self,prompt):
+        # for i,el in enumerate(prompt):
+        #     try:
+        #         prompt[i] = readfile(el)
+        #     except:
+        #         pass
+        lista_istruzioni = self.instructions_raw
+        instructions = [ [ es  for es in el.strip().split(" ") if len(es) > 0] for el in lista_istruzioni.strip().split("\n")]
+        res = self._execute_list(instructions, prompt)
+        return res
