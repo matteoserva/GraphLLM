07147aa3a67dfbde53db3dfee3eef519cce97228
matteo <matteo@matteo>
2024-05-22 12:51:17 +0200

    updating the exec demo

--- exec.py
+++ exec.py
@@ -6,10 +6,6 @@ from utils.formatter import Formatter,PromptBuilder
 
 from utils.executor import SequenceExecutor
 
-fn = "sequences/reddit_eval.txt"
-if len(sys.argv) > 1:
-    fn = sys.argv[1]
-
 client = Client()
 client.connect()
 
@@ -17,13 +13,11 @@ parameters = {}
 parameters["repeat_penalty"] = 1.0
 parameters["penalize_nl"] = False
 parameters["seed"] = -1
-client.set_parameters(parameters)
-
 
 seqExec = SequenceExecutor(client)
-execRow = seqExec.execRow
+seqExec.set_client_parameters(parameters)
+
 cl_args = seqExec.load_config(sys.argv[1:])
-print(cl_args)
 seqExec(cl_args[1:])
 
 
