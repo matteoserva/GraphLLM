13c9e8f95228afc88821353415c63fabbd9f48df
matteo <matteo@matteo>
2024-09-30 15:45:03 +0200

    test grammars

--- test/test_grammar.py
+++ test/test_grammar.py
@@ -4,11 +4,11 @@ import inspect
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir) 
+sys.path.insert(0, parentdir)
 
-from utils.grammar import load_grammar 
+from modules.grammar import load_grammar 
 
 a = load_grammar("grammars/json_response.txt")
 print(a)
-a = load_grammar("grammars/react.txt")
+a = load_grammar("grammars/react_text.txt")
 print(a)
