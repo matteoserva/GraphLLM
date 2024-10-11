075dc536cb38f118ecea9737a7cc0296293301c8
matteo serva <matteo.serva@gmail.com>
2024-10-11 16:54:33 +0200

    litegraph

---
+++ extras/litegraph.js/doc/api.js
@@ -0,0 +1,14 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ContextMenu",
+        "LGraph",
+        "LGraphCanvas",
+        "LGraphNode",
+        "LiteGraph"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
