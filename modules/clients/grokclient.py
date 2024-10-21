e21e1f967ad3d6cc13ebb7c1c6277660e915f4e1
matteo <matteo@matteo>
2024-10-21 14:16:01 +0200

    fixed groq client

--- modules/clients/grokclient.py
+++ modules/clients/grokclient.py
@@ -1,4 +1,5 @@
 from groq import Groq
+import copy
 
 class GrokClient():
 
@@ -10,22 +11,29 @@ class GrokClient():
         self.model_name="llama3-70b-8192"
         self.default_params={ "temperature":0.1, "max_tokens":1024//2, "stream":True, "stop":None}
 
-    def send_prompt(self,p,params=None):
+    def send_prompt(self,p,params=None, callback=None):
         if isinstance(p,list):
             messages = p
         else:
             messages = p.messages
-        print(messages)
-        params = self.default_params
+        #print(messages)
+        user_params = params
+        params = copy.deepcopy(self.default_params)
+        if params:
+            params["stop"] = user_params.get("stop",None)
         model_name = self.model_name
         completion = self.client.chat.completions.create( model=self.model_name, messages=messages,**params)
-        return self.ricevi(completion)
+        return self.ricevi(completion,callback)
 
-    def ricevi(self,completion):
+    def ricevi(self,completion,callback=None):
+        res = ""
         for chunk in completion:
-             #print(chunk)
+             
              val = chunk.choices[0].delta.content or ""
-             yield val
+             res += val
+             if callback:
+                callback(val)
+        return res
 
     def connect(self):
         if not self.client:
