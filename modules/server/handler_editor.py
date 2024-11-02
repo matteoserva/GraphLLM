94968133f26f1e052daf7676c28ccf2bcc9ffef3
matteo serva <matteo.serva@gmail.com>
2024-11-02 17:01:57 +0100

    refactored the static editor data handler

---
+++ modules/server/handler_editor.py
@@ -0,0 +1,85 @@
+import os
+
+SOURCES_PATH="modules/server"
+NODES_PATH="modules/gui_nodes"
+LITEGRAPH_PATH = "extras/litegraph.js"
+
+class EditorHandler():
+
+    def do_GET(self, server):
+        server.close_connection = True
+        http_path = server.path.split("?", 1)[0]
+        split_path = http_path.split("/", 2)
+        endpoint = split_path[1]
+        if endpoint in ["editor"] and len(split_path) < 3:
+            server.send_response(301)
+            server.send_header('Location', '/editor/')
+            server.end_headers()
+            return
+
+        if endpoint in ["editor"] and len(split_path[2]) == 0:  # index
+            filename = SOURCES_PATH + "/" + "index.html"
+
+            node_files = os.listdir(NODES_PATH)
+            node_files = [el for el in node_files if el.endswith(".js")]
+            node_files = sorted(node_files)
+
+            replaced_node_files = ['<script type="text/javascript" src="nodes/' + el + '"></script>' for el in node_files]
+            replaced_text = "\n".join(replaced_node_files)
+
+            content = open(filename, "rb").read()
+            content = content.replace(b"<!-- NODE_LIST_PLACEHOLDER -->", replaced_text.encode())
+
+            server.send_response(200)
+            server.send_header('Connection', 'close')
+            server.send_header('Content-type', 'text/html')
+            server.end_headers()
+            server.wfile.write(content)
+
+        elif endpoint in ["editor"]:
+            remaining = "index.html" if len(split_path[2]) == 0 else split_path[2]
+            print(remaining)
+            content = None
+
+            if remaining.startswith("nodes/"):
+                filename = NODES_PATH + "/" + split_path[2].split("/")[-1]
+                content = open(filename, "rb").read()
+
+            if not content:
+                filename = SOURCES_PATH + "/" + remaining
+                if os.path.exists(filename):
+                    content = open(filename, "rb").read()
+
+            if not content:
+                filename = LITEGRAPH_PATH + "/editor/" + remaining
+                if content is None and os.path.exists(filename):
+                    content = open(filename, "rb").read()
+
+            server.send_response(200)
+            server.send_header('Connection', 'close')
+            # self.send_header('Content-type', 'text/html')
+            server.end_headers()
+            server.wfile.write(content)
+        elif endpoint in ["src", "external", "css", "js", "imgs", "style.css", "examples"]:
+            remaining = "/index.html" if len(split_path) < 3 else "/" + split_path[2]
+
+            content = None
+            filename = LITEGRAPH_PATH + "/" + endpoint + remaining
+            print(filename)
+            if content is None and os.path.isfile(filename):
+                content = open(filename, "rb").read()
+            if content:
+                server.send_response(200)
+                # self.send_header('Content-type', 'text/html')
+                server.end_headers()
+                server.wfile.write(content)
+            else:
+                server.send_response(404)
+                server.send_header('Content-type', 'text/html')
+                server.end_headers()
+                server.wfile.write(b'404 - Not Found')
+        else:
+            server.send_response(404)
+            server.send_header('Content-type', 'text/html')
+            server.end_headers()
+            server.wfile.write(b'404 - Not Found')
--- modules/server/web_app.py
+++ modules/server/web_app.py
@@ -5,16 +5,13 @@ from functools import partial
 
 from modules.server.handler_exec import ExecHandler
 from modules.server.handler_model import ModelHandler
-
-SOURCES_PATH="modules/server"
-NODES_PATH="modules/gui_nodes"
-LITEGRAPH_PATH = "extras/litegraph.js"
-
+from modules.server.handler_editor import EditorHandler
 
 class HttpDispatcher:
 
     def __init__(self, *args, **kwargs):
         self.model = ModelHandler()
+        self.editor = EditorHandler()
 
     def do_GET(self,server):
         self.model.server = server
@@ -29,13 +26,7 @@ class HttpDispatcher:
         split_path = http_path.split("/",2)
 
         endpoint = split_path[1]
-        if endpoint in ["editor"] and len(split_path) < 3:
-            server.send_response(301)
-            server.send_header('Location','/editor/')
-            server.end_headers()
-            return
 
-        #print(http_path)
         if endpoint in ["graph"]:
             http_path = server.path.split("?", 1)[0]
             split_path = http_path.split("/", 2)
@@ -61,67 +52,9 @@ class HttpDispatcher:
                 server.wfile.write(b'404 - Not Found')
             pass
 
-        elif endpoint in ["editor"] and len(split_path[2]) == 0: #index
-            filename = SOURCES_PATH + "/" + "index.html"
-
-            node_files = os.listdir(NODES_PATH)
-            node_files = [el for el in node_files if el.endswith(".js")]
-            node_files = sorted(node_files)
-
-            replaced_node_files = ['<script type="text/javascript" src="nodes/' + el + '"></script>' for el in node_files]
-            replaced_text = "\n".join(replaced_node_files)
-
-            content = open(filename, "rb").read()
-            content = content.replace(b"<!-- NODE_LIST_PLACEHOLDER -->",replaced_text.encode())
-
-            server.send_response(200)
-            server.send_header('Connection', 'close')
-            server.send_header('Content-type', 'text/html')
-            server.end_headers()
-            server.wfile.write(content)
-
-        elif endpoint in ["editor"] :
-            remaining = "index.html" if len(split_path[2]) == 0 else split_path[2]
-            print(remaining)
-            content = None
-
-            if remaining.startswith("nodes/"):
-                filename = NODES_PATH + "/" + split_path[2].split("/")[-1]
-                content = open(filename, "rb").read()
-
-            if not content:
-                filename = SOURCES_PATH + "/" + remaining
-                if os.path.exists(filename):
-                    content = open(filename,"rb").read()
-
-            if not content:
-                filename = "extras/litegraph.js/editor/"  + remaining
-                if content is None and os.path.exists(filename):
-                    content = open(filename,"rb").read()
-
-            server.send_response(200)
-            server.send_header('Connection', 'close')
-                #self.send_header('Content-type', 'text/html')
-            server.end_headers()
-            server.wfile.write(content)
-        elif endpoint in ["src","external","css","js","imgs","style.css","examples"]:
-            remaining = "/index.html" if len(split_path) < 3 else "/" + split_path[2]
-            
-            content = None
-            filename = "extras/litegraph.js/" + endpoint + remaining
-            print(filename)
-            if content is None and os.path.isfile(filename):
-                content = open(filename,"rb").read()
-            if content:
-                server.send_response(200)
-                #self.send_header('Content-type', 'text/html')
-                server.end_headers()
-                server.wfile.write(content)
-            else:
-                server.send_response(404)
-                server.send_header('Content-type', 'text/html')
-                server.end_headers()
-                server.wfile.write(b'404 - Not Found')
+        elif endpoint in ["editor","src","external","css","js","imgs","style.css","examples"]:
+            handler = self.editor
+            return handler.do_GET(server)
         else:
              server.send_response(404)
              server.send_header('Content-type', 'text/html')
