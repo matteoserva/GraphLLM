6f758162653e91a7a0b621782e0681703a316cfc
matteo serva <matteo.serva@gmail.com>
2024-11-02 16:42:38 +0100

    http dispatcher

--- modules/server/web_app.py
+++ modules/server/web_app.py
@@ -11,45 +11,43 @@ NODES_PATH="modules/gui_nodes"
 LITEGRAPH_PATH = "extras/litegraph.js"
 
 
-class HttpHandler(BaseHTTPRequestHandler):
-    protocol_version = 'HTTP/1.1'
-    def __init__(self, model, *args, **kwargs):
-        self.model = model
-        self.model.server = self
+class HttpDispatcher:
 
-        super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        self.model = ModelHandler()
 
-    def do_GET(self):
-        self.close_connection = True
-        http_path = self.path.split("?",1)[0]
+    def do_GET(self,server):
+        self.model.server = server
+        server.close_connection = True
+        http_path = server.path.split("?",1)[0]
 
         if http_path == "/":
-            self.send_response(301)
-            self.send_header('Location','/editor/')
-            self.end_headers()
+            server.send_response(301)
+            server.send_header('Location','/editor/')
+            server.end_headers()
             return
         split_path = http_path.split("/",2)
 
         endpoint = split_path[1]
         if endpoint in ["editor"] and len(split_path) < 3:
-            self.send_response(301)
-            self.send_header('Location','/editor/')
-            self.end_headers()
+            server.send_response(301)
+            server.send_header('Location','/editor/')
+            server.end_headers()
             return
 
         #print(http_path)
         if endpoint in ["graph"]:
-            http_path = self.path.split("?", 1)[0]
+            http_path = server.path.split("?", 1)[0]
             split_path = http_path.split("/", 2)
             if len(split_path) < 3 or len(split_path[2]) < 1:
-                self.send_response(404)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                self.wfile.write(b'404 - Not Found')
+                server.send_response(404)
+                server.send_header('Content-type', 'text/html')
+                server.end_headers()
+                server.wfile.write(b'404 - Not Found')
 
             operation = split_path[2]
-            if self.headers.get("Upgrade", None) == "websocket":
-                wsExec = ExecHandler(self)
+            if server.headers.get("Upgrade", None) == "websocket":
+                wsExec = ExecHandler(server)
                 op = getattr(wsExec,operation)
                 res = op()
             elif hasattr(self.model, operation):
@@ -57,10 +55,10 @@ class HttpHandler(BaseHTTPRequestHandler):
                 res = op()
 
             else:
-                self.send_response(404)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                self.wfile.write(b'404 - Not Found')
+                server.send_response(404)
+                server.send_header('Content-type', 'text/html')
+                server.end_headers()
+                server.wfile.write(b'404 - Not Found')
             pass
 
         elif endpoint in ["editor"] and len(split_path[2]) == 0: #index
@@ -76,11 +74,11 @@ class HttpHandler(BaseHTTPRequestHandler):
             content = open(filename, "rb").read()
             content = content.replace(b"<!-- NODE_LIST_PLACEHOLDER -->",replaced_text.encode())
 
-            self.send_response(200)
-            self.send_header('Connection', 'close')
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write(content)
+            server.send_response(200)
+            server.send_header('Connection', 'close')
+            server.send_header('Content-type', 'text/html')
+            server.end_headers()
+            server.wfile.write(content)
 
         elif endpoint in ["editor"] :
             remaining = "index.html" if len(split_path[2]) == 0 else split_path[2]
@@ -101,11 +99,11 @@ class HttpHandler(BaseHTTPRequestHandler):
                 if content is None and os.path.exists(filename):
                     content = open(filename,"rb").read()
 
-            self.send_response(200)
-            self.send_header('Connection', 'close')
+            server.send_response(200)
+            server.send_header('Connection', 'close')
                 #self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write(content)
+            server.end_headers()
+            server.wfile.write(content)
         elif endpoint in ["src","external","css","js","imgs","style.css","examples"]:
             remaining = "/index.html" if len(split_path) < 3 else "/" + split_path[2]
             
@@ -115,43 +113,59 @@ class HttpHandler(BaseHTTPRequestHandler):
             if content is None and os.path.isfile(filename):
                 content = open(filename,"rb").read()
             if content:
-                self.send_response(200)
+                server.send_response(200)
                 #self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                self.wfile.write(content)
+                server.end_headers()
+                server.wfile.write(content)
             else:
-                self.send_response(404)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                self.wfile.write(b'404 - Not Found')
+                server.send_response(404)
+                server.send_header('Content-type', 'text/html')
+                server.end_headers()
+                server.wfile.write(b'404 - Not Found')
         else:
-             self.send_response(404)
-             self.send_header('Content-type', 'text/html')
-             self.end_headers()
-             self.wfile.write(b'404 - Not Found')
-    def do_POST(self):
-        self.close_connection = True
-        http_path = self.path.split("?", 1)[0]
+             server.send_response(404)
+             server.send_header('Content-type', 'text/html')
+             server.end_headers()
+             server.wfile.write(b'404 - Not Found')
+    def do_POST(self,server):
+        self.model.server = server
+        server.close_connection = True
+        http_path = server.path.split("?", 1)[0]
         split_path = http_path.split("/", 2)
         operation = split_path[2]
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
+        content_length = int(server.headers['Content-Length'])
+        post_data = server.rfile.read(content_length)
         if hasattr(self.model,operation):
 
             op = getattr(self.model,operation)
             res = op(post_data)
 
         else:
-            self.send_response(404)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write(b'404 - Not Found')
+            server.send_response(404)
+            server.send_header('Content-type', 'text/html')
+            server.end_headers()
+            server.wfile.write(b'404 - Not Found')
         pass
 
+class HttpServerWrapper(BaseHTTPRequestHandler):
+    protocol_version = 'HTTP/1.1'
+
+    def __init__(self, dispatcher, *args, **kwargs):
+        self.dispatcher = dispatcher
+        super().__init__(*args, **kwargs)
+
+    def do_GET(self):
+        return self.dispatcher.do_GET(self)
+
+    def do_POST(self):
+        return self.dispatcher.do_POST(self)
+
+
+
 def run():
-    model_handler = ModelHandler()
-    http_handler = partial(HttpHandler, model_handler)
-    http_server = HTTPServer(('0.0.0.0', 8008), http_handler)
+    http_dispatcher = HttpDispatcher()
+    http_wrapper = partial(HttpServerWrapper, http_dispatcher)
+    http_server = HTTPServer(('0.0.0.0', 8008), http_wrapper)
     print("server listening at http://localhost:8008/")
 
     def handler(signal_received, frame):
