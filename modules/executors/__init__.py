7b1f3eb924c13829db4a29807282e25653ea6c39
matteo serva <matteo.serva@gmail.com>
2024-11-03 16:43:52 +0100

    removing __init__ files from executor directories

--- modules/executors/__init__.py
+++
@@ -1,7 +0,0 @@
-#from modules.executors.other.node_other import *
-#from modules.executors.llm.node_llm import *
-#from modules.executors.agent.node_agent import *
-#from modules.executors.tools.node_tools import *
-#from modules.executors.inputs.node_inputs import *
-#from .node_exec import *
-#from .tts import *
--- modules/graph/executor_factory.py
+++ modules/graph/executor_factory.py
@@ -1,6 +1,10 @@
 
 from modules.logging.logger import Logger
 import sys, inspect
+from glob import glob
+import os
+
+from modules.executors import *
 from modules.executors.common import GenericExecutor
 from modules.executors.agent.node_agent import AgentController
 from modules.executors.tools.node_tools import ToolExecutor, LlamaTool
@@ -11,30 +15,24 @@ def default_logger(*args,**kwargs):
 from pkgutil import iter_modules
 from importlib import import_module
 
-def find_submodules(modules, python_name = "modules.executors"):
-    found_executors = []
-    filtered_modules = [el for el in modules if not el.ispkg]
-    for module_tuple in filtered_modules:
-
-        submodule_name = "{}.{}".format(python_name, module_tuple.name)
-        module = import_module(submodule_name)
-        attributes = [getattr(module, attribute_name) for attribute_name in dir(module)]
-        generic_executors = [attribute for attribute in attributes if inspect.isclass(attribute) and issubclass(attribute, GenericExecutor) and hasattr(attribute, "node_type")]
-        found_executors.extend(generic_executors)
-    return found_executors
-
 def _load_executor_classes():
     from ..graph.graph_executor import GraphExecutor
-    found_executors = []
-    executors = import_module("modules.executors")
-    toplevel_modules = [el for el in iter_modules(executors.__path__)]
-    found_executors.extend(find_submodules(toplevel_modules))
+    modules = [el[:-3] for el in glob("*.py",root_dir="modules/executors" ) if not el.startswith("_")]
 
-    for module_tuple in [el for el in toplevel_modules if el.ispkg]:
-        python_name = "modules.executors.{}".format(module_tuple.name)
-        submodule = import_module(python_name)
-        subdirs = [el for el in iter_modules(submodule.__path__)]
-        found_executors.extend(find_submodules(subdirs, python_name))
+    for sub_name, sub_path in [ (f.name, f.path) for f in os.scandir("modules/executors") if f.is_dir()]:
+        sub_modules = [el[:-3] for el in glob("*.py",root_dir=sub_path ) if not el.startswith("_")]
+        sub_modules = [sub_name + "." + el for el in sub_modules]
+        modules.extend(sub_modules)
+    modules = ["modules.executors." + el for el in modules]
+
+    found_executors = []
+    for m in modules:
+        module = import_module(m)
+        attributes = [getattr(module, attribute_name) for attribute_name in dir(module)]
+        generic_executors = [attribute for attribute in attributes if
+                             inspect.isclass(attribute) and issubclass(attribute, GenericExecutor) and hasattr(attribute, "node_type")]
+        found_executors.extend(generic_executors)
+
 
     executors_map = {el.node_type:el for el in found_executors}
 
