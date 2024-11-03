b6cce394448e20bb4432758cf4350fe23cdd87ec
matteo serva <matteo.serva@gmail.com>
2024-11-03 15:49:01 +0100

    dynamically load executor classes

--- modules/graph/executor_factory.py
+++ modules/graph/executor_factory.py
@@ -1,6 +1,4 @@
-#
-from modules.executors import *
-from modules.executors.node_exec import *
+
 from modules.logging.logger import Logger
 import sys, inspect
 from modules.executors.common import GenericExecutor
@@ -8,15 +6,37 @@ from modules.executors.common import GenericExecutor
 def default_logger(*args,**kwargs):
     print(*args,**kwargs)
 
+from pkgutil import iter_modules
+from importlib import import_module
+
+def find_submodules(modules, python_name = "modules.executors"):
+    found_executors = []
+    filtered_modules = [el for el in modules if not el.ispkg]
+    for module_tuple in filtered_modules:
+
+        submodule_name = "{}.{}".format(python_name, module_tuple.name)
+        module = import_module(submodule_name)
+        attributes = [getattr(module, attribute_name) for attribute_name in dir(module)]
+        generic_executors = [attribute for attribute in attributes if inspect.isclass(attribute) and issubclass(attribute, GenericExecutor) and hasattr(attribute, "node_type")]
+        found_executors.extend(generic_executors)
+    return found_executors
+
 def _load_executor_classes():
-    res = inspect.getmembers(sys.modules[__name__], inspect.isclass)
-    res = [el[1] for el in res if issubclass(el[1], GenericExecutor)]
-    res = {el.node_type: el for el in res if hasattr(el, "node_type")}
-    return res
+    found_executors = []
+    executors = import_module("modules.executors")
+    toplevel_modules = [el for el in iter_modules(executors.__path__)]
+    found_executors.extend(find_submodules(toplevel_modules))
+
+    for module_tuple in [el for el in toplevel_modules if el.ispkg]:
+        python_name = "modules.executors.{}".format(module_tuple.name)
+        submodule = import_module(python_name)
+        subdirs = [el for el in iter_modules(submodule.__path__)]
+        found_executors.extend(find_submodules(subdirs, python_name))
+
+    executors_map = {el.node_type:el for el in found_executors}
+    return executors_map
 
 _available_executors = _load_executor_classes()
-
-
 
 class ExecutorFactory:
     
