c342ee3dd786f5af7b9e391b139cd440c36b6f6b
matteo serva <matteo.serva@gmail.com>
2024-11-10 13:20:37 +0100

    better error handling when a dependency is missing

--- modules/executors/__init__.py
+++ modules/executors/__init__.py
@@ -7,7 +7,13 @@ from importlib import import_module
 
 from modules.executors.common import GenericExecutor, BaseGuiParser
 
-def _get_all_submodules(node_class=GenericExecutor):
+_cached_classes = None
+
+def _get_all_classes():
+    global _cached_classes
+    if _cached_classes:
+        return _cached_classes
+
     sub_path = "modules/executors"
     modules = [el[len(sub_path) + 1:-3] for el in glob(sub_path + "/*.py") if not el.startswith("_")]
 
@@ -17,17 +23,24 @@ def _get_all_submodules(node_class=GenericExecutor):
         modules.extend(sub_modules)
     modules = ["modules.executors." + el for el in modules]
 
-    # now filter
-    found_executors = []
+    found_classes = []
     for m in modules:
         try:
             module = import_module(m)
             attributes = [getattr(module, attribute_name) for attribute_name in dir(module)]
-            generic_executors = [attribute for attribute in attributes if
-                             inspect.isclass(attribute) and issubclass(attribute, node_class)]
-            found_executors.extend(generic_executors)
+            generic_executors = [attribute for attribute in attributes if inspect.isclass(attribute)]
+            found_classes.extend(generic_executors)
         except Exception as e:
-            print("Exception while loading module",m, " : ", e)
+            print("Exception while loading module",m, " : ", e,file=sys.stderr)
+
+    _cached_classes = found_classes
+    return found_classes
+
+def _get_all_submodules(node_class=GenericExecutor):
+
+    found_classes = _get_all_classes()
+    # now filter
+    found_executors = [el for el in found_classes if issubclass(el, node_class) ]
 
     return found_executors
 
