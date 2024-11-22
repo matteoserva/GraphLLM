55d545eff23f8026ca4594d1e07e99246f7c3c9d
matteo serva <matteo.serva@gmail.com>
2024-11-22 15:47:13 +0100

    refactoring

--- modules/executors/llm/llm.js
+++ modules/executors/llm/llm.js
@@ -77,68 +77,7 @@
 
 
 
-        /****
-     * 
-     * 
-     * 
-     * chat history NODE
-     * 
-     * */
-    function MyChatHistoryNode()
-    {
-    this.addOutput("out","string");
-    //this.addInput("in","string");
 
-    this.container = new DivContainer(this)
-    this.addCustomWidget( this.container);
-    var listWidget = this.container.addWidget("list","Parameters",{ property: "parameters"})
-    listWidget.makeCellName = function(index)
-    {
-        if(index == 0)
-        {
-                return "system";
-        }
-        else if(index % 2)
-        {
-                return "user";
-        }
-        else
-        {
-                return "assistant"
-        }
-        return index+1
-    }
-    listWidget.reset()
-    //this.container.addWidget("textarea","Parameters",{ property: "parameters"})
-    this.properties = {  };
-    }
-
-    //name to show
-    MyChatHistoryNode.title = "Chat History";
-
-    //register in the system
-    LiteGraph.registerNodeType("llm/chat_history", MyChatHistoryNode );
-    
-    MyChatHistoryNode.prototype.onStart = function()
-    {
-        this.saved_history = JSON.parse(JSON.stringify(this.properties["parameters"]))
-        if((this.saved_history.length % 2) != 1)
-        {
-            this.saved_history.push("")   
-        }
-    }
-    
-    MyChatHistoryNode.prototype.onExecute = function()
-    {
-        var A = this.getInputData(0);
-        if( A !== undefined )
-        {
-            var new_history = JSON.parse(JSON.stringify(this.saved_history))
-            var message_index = new_history.length-1
-            new_history[message_index] =this.saved_history[message_index] + A
-            this.container.setValue("parameters",new_history)
-        }
-    }
     
 
     /* ----------------- -------------- */
---
+++ modules/executors/network/websockets_gui.py
@@ -0,0 +1,36 @@
+from modules.executors.common import BaseGuiNode
+
+
+class WebsocketClientGui(BaseGuiNode):
+    node_title="Websocket client"
+
+    def __init__(self):
+        pass
+
+
+    def buildNode(self):
+        builder = self
+        builder._reset()
+        builder._addInput("send","string");
+        builder._addOutput("receive","string");
+        
+        builder._addCustomWidget ("text_input","address",{ "property": "address"})
+        builder._setPath("network/websocket_client")
+        
+
+class WebsocketServerGui(BaseGuiNode):
+    node_title="Websocket server"
+
+    def __init__(self):
+        pass
+
+
+    def buildNode(self):
+        builder = self
+        builder._reset()
+        builder._addInput("send","string");
+        builder._addOutput("receive","string");
+        
+        options = { "min": 1, "max": 65535, "step": 10, "precision": 0, "property": "port" }
+        builder._addStandardWidget("number","port", 8765, None, options )
+        builder._setPath("network/websocket_server")
