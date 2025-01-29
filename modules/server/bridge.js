823117051e9bd2ef4d824b765ae07030f2924bf2
matteo serva <matteo.serva@gmail.com>
2025-01-29 12:44:34 +0100

    refactoring

--- modules/server/bridge.js
+++ modules/server/bridge.js
@@ -32,6 +32,8 @@ class WebBrige {
     this.select = document.querySelector("div.litegraph .selector .selector-main")
 
     this.audio = new Audio();
+
+    this.graph_handler = new GraphHandler(this.graph,this.canvas)
   }
 
   setDefaultConnectionEndpoints()
@@ -40,141 +42,16 @@ class WebBrige {
     LiteGraph.slot_types_default_in.string=["input/text_input"]
   }
 
-  processMouseDown(e)
-  {
-    if (this.canvas.selected_group)
-    {
-        if (this.canvas.selected_group.collapse_state && this.canvas.selected_group.collapse_state.is_collapsed &&this.canvas.selected_group_resizing)
-        {
-            this.canvas.selected_group_resizing = false
-            this.canvas.selected_group.recomputeInsideNodes();
-        }
-    }
-    if (this.canvas.node_dragged)
-    {
-        var draggable = true;
-        for (let el in this.graph._groups)
-        {
-            var group = this.graph._groups[el]
-            if(group.collapse_state && group.collapse_state.is_collapsed)
-            {
-                if(!this.canvas.node_dragged)
-                {
-                    console.log("strano")
-                }
 
-                var managed_nodes = Object.keys(group.collapse_state.node_info)
-                if( managed_nodes.includes('' + this.canvas.node_dragged.id) )
-                {
-                    draggable = false
-                }
-            }
-        }
-        if(!draggable)
-        {
-            this.canvas.node_dragged = null
-            }
-
-    }
-
-
-  }
-  processMouseUp(e)
-  {
-
-          var now = LiteGraph.getTime();
-        var previousClick = this.previousClick || [0,0]
-        this.previousClick = [now,this.canvas.last_mouseclick].concat( [previousClick[0], previousClick[1]])
-        var total = this.previousClick[0] - this.previousClick[3]
-        var t1 = (this.previousClick[0] - this.previousClick[1])
-        var t2 = (this.previousClick[2] - this.previousClick[3])
-        var is_double_click = (total < 300) && (t1 > 1) && (t2 > 1) && (t1 < 100) && (t2 < 100)
-
-		var is_primary = (e.isPrimary === undefined || e.isPrimary);
-        is_double_click = is_double_click && is_primary;
-        var should_handle = this.canvas.selected_group && is_double_click
-        if(this.canvas.selected_group)
-        {
-            this.recomputeInsideNodes(this.canvas.selected_group)
-
-        }
-        if(this.canvas.node_dragged || this.canvas.resizing_node)
-        {
-            let node = this.canvas.node_dragged || this.canvas.resizing_node
-            this.recomputeOutsideGroup(node)
-        }
-
-        if(should_handle)
-        {
-            var group = this.canvas.selected_group
-            if (group.collapse_state === undefined)
-            {
-                group.collapse_state = { is_collapsed: false, node_info: new Object()}
-            }
-            if (group.collapse_state.is_collapsed)
-            {
-                group.collapse_state.is_collapsed = false
-                for (var i = 0; i < group._nodes.length; ++i) {
-                    var node = group._nodes[i];
-                    var id = '' + node.id
-
-                    if(Object.keys(group.collapse_state.node_info).includes(id))
-                    {
-                        var node_info = group.collapse_state.node_info[id]
-                        node.pos = [group.pos[0]+node_info.pos[0], group.pos[1] + node_info.pos[1]]
-                        if (node.flags.collapsed && !node_info.collapsed)
-                        {
-                            node.collapse()
-                        }
-
-                    }
-                }
-                group.size = group.collapse_state.group_size
-                group.color = group.collapse_state.color
-                //
-            }
-            else
-            {
-                for (var i = 0; i < group._nodes.length; ++i) {
-                    var node = group._nodes[i];
-                    var delta_pos = [ node.pos[0] - group.pos[0],  node.pos[1] - group.pos[1]]
-                    var new_pos = [ group.pos[0] + 20,   group.pos[1] + 70]
-                    group.collapse_state.node_info[node.id] = {id: node.id, size: node.size, pos: delta_pos, collapsed: node.flags.collapsed}
-
-                    if(!node.flags.collapsed)
-                    {
-                        node.collapse()
-                    }
-                    node.pos = new_pos
-
-
-                }
-                var internal_nodes = group._nodes.map((n) => ''+n.id)
-                // remove nodes not in group
-                var node_info = group.collapse_state.node_info
-                node_info = Object.keys(node_info).filter((k) => internal_nodes.includes(k)).reduce((cur,key) => { return Object.assign(cur, { [key]: node_info[key] })}, {});
-                group.collapse_state.node_info = node_info
-                group.collapse_state.group_size = [...group.size]
-                group.size = [180,110]
-                group.collapse_state.color = group.color
-                group.color = "#606060"
-                group.collapse_state.is_collapsed = true
-
-            }
-        }
-        //console.log("group: " + this.canvas.selected_group + "   double " + is_double_click + "   time: "
-        //+ (now - this.canvas.last_mouseclick), " p " + this.previousClick + "  t " + [total,t1,t2])
-
-    return false;
-  }
 
   connect() {
     this.setDefaultConnectionEndpoints()
+    this.graph_handler.connect()
 
     this.canvas.allow_searchbox = false
 
-    this.canvas.onMouseDown = this.processMouseDown.bind(this)
-    LiteGraph.pointerListenerAdd(document,"up", this.processMouseUp.bind(this),true);
+
+
     this.cb_as = this.graph.onAfterStep;
     let cb_bs = this.graph.onBeforeStep;
     let that = this
@@ -220,69 +97,11 @@ class WebBrige {
         }
     });
 
-    var orig = LGraphGroup.prototype.recomputeInsideNodes
-    LGraphGroup.prototype.recomputeInsideNodes = function() {
-        var group = this
-        that.recomputeInsideNodes(group,orig)
-    };
+
 
   }
 
-  recomputeOutsideGroup(node)
-  {
-    var node_bounding = new Float32Array(4);
-    node.getBounding(node_bounding);
 
-    if (node.parent_group)
-    {
-        if (LiteGraph.overlapBounding(node.parent_group._bounding, node_bounding)) {
-            return;
-        }
-
-    }
-
-    var found_group = null
-    for (var i = 0; i < this.graph._groups.length; ++i) {
-        var group = this.graph._groups[i]
-        if (!LiteGraph.overlapBounding(group._bounding, node_bounding)) {
-            continue;
-        }
-        found_group = group
-    }
-
-    if (found_group)
-    {
-        node.parent_group = found_group
-        console.log("outside group")
-    }
-
-  }
-
-  recomputeInsideNodes(group, orig)
-  {
-    group._nodes.length = 0;
-    var nodes = this.graph._nodes;
-    var node_bounding = new Float32Array(4);
-
-    for (var i = 0; i < nodes.length; ++i) {
-        var node = nodes[i];
-        node.getBounding(node_bounding);
-        if (!LiteGraph.overlapBounding(group._bounding, node_bounding)) {
-            continue;
-        } //out of the visible area
-        if(node.parent_group && node.parent_group != group)
-        {
-            var parent_group = node.parent_group
-            if (LiteGraph.overlapBounding(parent_group._bounding, node_bounding)) {
-                continue;
-            }
-            console.log("stealing")
-        }
-        group._nodes.push(node);
-        node.parent_group = group
-    }
-    console.log("recompute")
-  }
 
   loadList()
   {
@@ -621,7 +440,7 @@ class WebBrige {
     }
     for (var i = 0; i < this.graph._nodes.length; ++i) {
         var node = this.graph._nodes[i];
-        this.recomputeOutsideGroup(node)
+        this.graph_handler.recomputeOutsideGroup(node)
     }
   }
 
