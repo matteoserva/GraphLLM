cca2a6ee71e92fcab756aea85a408f4f7b97d4fd
matteo <matteo@naspc.lan>
2025-04-07 16:58:30 +0200

    handle ctrl+s

--- modules/server/bridge.js
+++ modules/server/bridge.js
@@ -110,6 +110,9 @@ class WebBrige {
 
             }
         }
+		else if(event.ctrlKey && event.key == "s") {
+            this.save()
+        }
         else
         {
             input_handled = false
