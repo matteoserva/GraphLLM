88189056fb04b8166befba6b8e50b2fb1be71a9c
matteo serva <matteo.serva@gmail.com>
2025-04-14 18:27:41 +0200

    fixed stub tools

--- modules/tool_call/tool_fake_weather.py
+++ modules/tool_call/tool_fake_weather.py
@@ -4,12 +4,12 @@ class FakeWeather(GenericTool):
 	tool_name = "fake weather"
 	properties = {"default": False, "priority": 10}
 
-	def get_current_temperature(self, location):
+	def get_current_temperature(self, location, unit="celsius"):
 		"""Get current temperature at a location."""
 		res = {"temperature": 26.1, "location": "San Francisco, CA, USA", "unit": "celsius"}
 		return res
 
-	def get_temperature_date(self, location, date):
+	def get_temperature_date(self, location, date, unit="celsius"):
 		"""Get temperature at a location and date."""
 		res = {"temperature": 25.9, "location": "San Francisco, CA, USA", "date": "2024-10-01", "unit": "celsius"}
 		return res
