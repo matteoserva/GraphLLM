1f53f5179e03589feb64ebac1907b9d6a189ac41
matteo serva <matteo.serva@gmail.com>
2025-04-21 18:10:18 +0200

    more fixes to reddit parser

--- extras/scraper/redditFixer.js
+++ extras/scraper/redditFixer.js
@@ -47,7 +47,8 @@ function removeAds()
 {
     document.querySelectorAll("shreddit-async-loader").forEach(el => {el.parentNode.removeChild(el) });
     document.querySelectorAll("shreddit-comments-page-ad").forEach(el => {el.parentNode.removeChild(el) });
-    document.querySelectorAll("shreddit-comments-tree-ad").forEach(el => {el.parentNode.removeChild(el) });
+    document.querySelectorAll("shreddit-comment-tree-ad").forEach(el => {el.parentNode.removeChild(el) });
+    
     document.querySelectorAll("shreddit-dynamic-ad-link").forEach(el => {el.parentNode.removeChild(el) });
 }
 
@@ -62,19 +63,34 @@ function sleep(ms) {
 async function fix_reddit_new_post()
 {
     document.querySelectorAll("faceplate-partial[loading=action]").forEach(el => {el.click()});
+    document.querySelectorAll("shreddit-post button").forEach(el => {el.click()});
     window.onFetchCompleted = function() {
         removeAds()
         document.querySelectorAll("faceplate-partial#top-level-more-comments-partial").forEach(el => {el.parentNode.removeChild(el)});
         removeNode("shreddit-title")
         document.querySelectorAll("div[id='next-comment']").forEach(el => {el.parentNode.removeChild(el) });
         document.querySelectorAll("main > shreddit-post").forEach(el => {replaceWith(el,"div")});
+        document.querySelectorAll("faceplate-loader").forEach(el => {el.parentNode.removeChild(el) });
+        document.querySelectorAll("faceplate-partial").forEach(el => {el.parentNode.removeChild(el) });
+        
+        document.querySelectorAll("faceplate-batch").forEach(el => {el.parentNode.outerHTML = el.innerHTML});
+        document.querySelectorAll("shreddit-comment-tree-stats").forEach(el => {el.parentNode.removeChild(el) });
+        
+        while(document.querySelectorAll("shreddit-comment-tree").length > 0)
+        {
+            replaceNode("shreddit-comment-tree")
+            while(document.querySelectorAll("shreddit-comment").length > 0){         replaceNode("shreddit-comment") }
+            //document.querySelectorAll("shreddit-comment").forEach(el => {replaceComment(el,"div")});
+        }
+        
+        //document.querySelectorAll("faceplate-batch").forEach(el => {replaceWith(el,"div")});
+        /*document.querySelectorAll("shreddit-comment").forEach(el => {replaceComment(el,"div")});
         document.querySelectorAll("shreddit-comment").forEach(el => {replaceComment(el,"div")});
         document.querySelectorAll("shreddit-comment").forEach(el => {replaceComment(el,"div")});
-        document.querySelectorAll("shreddit-comment").forEach(el => {replaceComment(el,"div")});
-        document.querySelectorAll("shreddit-comment").forEach(el => {replaceComment(el,"div")});
+        
+        
         document.querySelectorAll("shreddit-comment-tree").forEach(el => {d = document.createElement("div"); d.innerHTML = el.innerHTML; el.replaceWith(d) });
-        document.querySelectorAll("shreddit-comment-tree").forEach(el => {d = document.createElement("div"); d.innerHTML = el.innerHTML; el.replaceWith(d) });
-        document.querySelectorAll("shreddit-comment-tree").forEach(el => {d = document.createElement("div"); d.innerHTML = el.innerHTML; el.replaceWith(d) });
+        document.querySelectorAll("shreddit-comment-tree").forEach(el => {d = document.createElement("div"); d.innerHTML = el.innerHTML; el.replaceWith(d) });*/
         removeAds()
         
     }
@@ -83,9 +99,11 @@ async function fix_reddit_new_post()
 
 if (second_level == "reddit")
 {
-     
-  
-
+      var isOldPost = document.querySelectorAll("div[class='commentarea']").length > 0;
+      var isNewPost = document.querySelectorAll("main > shreddit-post").length > 0;
+      var isOldList = document.querySelectorAll("div[data-context=listing]").length > 0;
+      var isNewList = document.querySelectorAll("main shreddit-feed").length > 0;
+      console.log("old post", isOldPost, "new post", isNewPost, "old list", isOldList, "new list", isNewList);
       //old subreddit post
       //document.querySelectorAll("form.usertext > div").forEach(el => {p =el.parentNode; p.removeChild(el); p.parentNode.append(el)}); 
 
@@ -93,7 +111,8 @@ if (second_level == "reddit")
       document.querySelectorAll("div[class='commentarea'] div[data-subreddit]").forEach(el => {p = el.parentNode; el.parentNode.removeChild(el); p.parentNode.parentNode.appendChild(el)});
       // replace links before removing nodes. So it works for the list of links too
       document.querySelectorAll("div[data-subreddit] a").forEach(el => {el.replaceWith("["+el.innerText+"]("+el.href+")");});
-      document.querySelectorAll("div[data-subreddit]").forEach(el => {el.replaceWith(el.innerText)});
+      //document.querySelectorAll("div[data-subreddit]").forEach(el => {el.replaceWith(el.innerText);});
+      document.querySelectorAll("div.content").forEach(el => {el.innerText = el.innerText;});
 
     // new reddit
 
@@ -110,8 +129,8 @@ if (second_level == "reddit")
     //
     
     // new reddit single post
-    var isPost = document.querySelectorAll("main > shreddit-post").length > 0
-    if (isPost)
+    
+    if (isNewPost)
     {
         
         fix_reddit_new_post()
