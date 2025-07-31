bd08c9bf26423929362b97eb7edca3fe23be4a07
matteo <matteo@naspc.lan>
2025-07-31 14:02:20 +0200

    experimental python console

--- modules/fake_python.py
+++ modules/fake_python.py
@@ -9,7 +9,7 @@ import sys
 import ast
 import tempfile
 
-class fake_method:
+class _fake_method:
     def __init__(self,method_name):
         self.method_name = method_name
 
@@ -28,7 +28,7 @@ class fake_method:
     def __call__(self, *args, **kwargs):
         return self._call_method(self.method_name,*args, **kwargs)
 
-class fake_module:
+class _fake_module:
     def __init__(self,module_name,module_attrs, subimports):
         self.module_name = module_name
         self.mod = importlib.import_module(module_name)
@@ -39,7 +39,7 @@ class fake_module:
         if isinstance(m,types.ModuleType):
             newName = self.module_name + "." + key
             if newName in self.subimports:
-                return fake_module(newName,self.subimports[newName],self.subimports)
+                return _fake_module(newName,self.subimports[newName],self.subimports)
         return partial( self._wrapper_method, self.module_name, key)
 
     def remove (self, name):
@@ -50,7 +50,7 @@ class fake_module:
             m = getattr(self.mod,funcname)
             return m(*args, **kwargs)
 
-def my_exec(code, globals, locals):
+def _my_exec(code, globals, locals):
     a = ast.parse(code)
     last_expression = None
     if a.body:
@@ -68,13 +68,13 @@ def my_exec(code, globals, locals):
             pass
 
 
-class PythonInterpreter:
+class FakePython:
     def __init__(self):
         self.safe_builtins = ["int", "float","sum", "max", "min", "abs", "range", "str", "print","dir","len","next","enumerate"]
         self.safe_imports = ["re","sympy","numpy","datetime","bs4","requests","webbrowser","json","numpy.polynomial.polynomial","math","scipy.optimize"]
         self.fake_imports = {"os":["listdir","path"]}
         self.fake_subimports = {"os.path":["getsize","isfile"]}
-        self.fake_builtins = {"open":fake_method}
+        self.fake_builtins = {"open":_fake_method}
         pass
 
     def fakeimport(self, name, *args,**kwargs):
@@ -84,24 +84,34 @@ class PythonInterpreter:
             res = importlib.import_module(name)
             return res
         if name in self.fake_imports:
-            return fake_module(name,self.fake_imports[name],self.fake_subimports)
+            return _fake_module(name,self.fake_imports[name],self.fake_subimports)
 
-    def execute(self,code, context):
+    def makeGlobals(self):
         current_builtins = {}
         for el in self.safe_builtins:
             current_builtins[el] = getattr(builtins, el)
         for el in self.fake_builtins:
-            current_builtins[el] = fake_method(el)
+            current_builtins[el] = _fake_method(el)
         globalsParameter = {'__builtins__': current_builtins}
         globalsParameter['__builtins__']["__import__"] = self.fakeimport
         globalsParameter['__builtins__']["__name__"] = "__main__"
+        return globalsParameter
+
+
+
+class PythonInterpreter:
+    def __init__(self):
+        self.fake_python = FakePython()
+
+    def execute(self,code, context):
+        globalsParameter = self.fake_python.makeGlobals()
         for el in context:
             globalsParameter[el] = context[el]
         f = StringIO()
         with redirect_stdout(f):
             #alt_s = my_exec(code, globalsParameter, globalsParameter)
             try:
-                alt_s = my_exec(code, globalsParameter, globalsParameter)
+                alt_s = _my_exec(code, globalsParameter, globalsParameter)
             except Exception as e:
                 raise
             #    traceback.print_exc()
@@ -114,3 +124,71 @@ class PythonInterpreter:
         if not s and alt_s:
             s = str(alt_s)
         return s
+
+
+import code
+import io
+class PythonConsole:
+    def __init__(self):
+        self.fake_python = FakePython()
+        self._console = None
+        self._stdout_capture = io.StringIO()
+        self._stderr_capture = io.StringIO()
+    
+    def __pprint(self, *args,**kwargs):
+        kwargs["file"] = self._stdout_capture
+        print(*args,**kwargs)
+    
+    
+    def reset(self):
+        globalsParameter = self.fake_python.makeGlobals()
+        globalsParameter['__builtins__']["print"] = self.__pprint
+        self._console = code.InteractiveConsole(locals=globalsParameter)
+        self._console.write = self._stderr_capture.write
+        self._last_retval = False
+    
+    def _pushline(self, code):
+        if code.startswith(">>> ") or code.startswith("... "):
+            code = code[4:]
+        elif code.startswith(">>>") or code.startswith("..."):
+            code = code[3:]
+        retval = self._console.push(code)
+        
+        output = self._stdout_capture.getvalue()
+        error = self._stderr_capture.getvalue()
+        
+        # Reset the IO buffers for the next call
+        self._stdout_capture.seek(0)
+        self._stdout_capture.truncate(0)
+        self._stderr_capture.seek(0)
+        self._stderr_capture.truncate(0)
+        
+        prompt = "... " if self._last_retval else ">>> "
+        self._last_retval = retval
+            
+        
+        combined_output = prompt + code + "\n" + error + output
+        
+        return combined_output
+        
+    def push(self, code):
+        results = [self._pushline(el) for el in code.split("\n")]
+        textout = "".join(results)
+        print(textout,end="")
+        return textout
+            
+        
+
+code1 = """
+print('ciao')
+for i in range(4):
+    pricnt(i)
+
+print('ciao',end="")
+"""
+
+console = PythonConsole()
+console.reset()
+console.push(code1)
+console.push("a = 3")
+console.push("print(a)")
