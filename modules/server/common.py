461adcd80461a442fa7320244143d89c9c0b65a1
matteo serva <matteo.serva@gmail.com>
2025-08-01 13:44:23 +0200

    fix spurious error when server 404

--- modules/server/common.py
+++ modules/server/common.py
@@ -4,4 +4,4 @@ from  urllib.error import HTTPError
 
 class HandlerException(HTTPError):
     def __init__(self, code=404):
-        super().__init__(code=code, url="", msg="", hdrs="", fp="")
+        super().__init__(code=code, url="", msg="", hdrs="", fp=None)
